0808a586ded9dbd6c9a44353e8b558488c6b5042
Ismael <ismael.lima2000@outlook.com.br>
2024-05-10 17:48:13 -0300

    Finalização da ovelha.

--- Assets/Scripts/EnemyChase.cs
+++ Assets/Scripts/EnemyChase.cs
@@ -5,12 +5,22 @@ using UnityEngine.AI;
 
 public class EnemyChase : MonoBehaviour {
     [SerializeField] public NavMeshAgent enemy;
+    
     private GameObject playerGameObject;
+
     private bool isChasing = true;
+    private Animator animator;
+    private float distAtk;
+    [SerializeField] private float distLimit;
+    
     
     // Start is called before the first frame update
     void Start() {
         playerGameObject = GameObject.Find("Player");
+        animator = GetComponent<Animator>();
+
+        
+        
     }
 
     // Update is called once per frame
@@ -18,12 +28,34 @@ public class EnemyChase : MonoBehaviour {
         // Verifica se o GameObject do Player está ativo antes de acessá-lo
         if (playerGameObject != null && playerGameObject.activeSelf && isChasing) {
             enemy.SetDestination(playerGameObject.transform.position);
+
+            
         } else {
             enemy.ResetPath();
         }
     }
-
-    public void setIsChasing(bool b) {
+                
+        public void setIsChasing(bool b) {
         isChasing = b;
     }
+   
+
+
+
+    // public void AtaqueVilao(){
+    //     if(playerGameObject != null && Vector3.Distance(transform.position, playerGameObject.transform.position) <= distAtack){
+    //         animator.SetTrigger("Ataque");
+    //         atack = true;
+    //     }else{
+    //         animator.SetBool("Ataque", false);
+    //     }
+
+    //     if(atack){
+    //         enemy.speed = 0;
+    //         enemy.isStopped = true;
+    //     }else{
+    //         enemy.speed = 10;
+    //         enemy.isStopped = false;
+    //     }
+    // }
 }
--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -7,19 +7,34 @@ public class EnemyDamage : MonoBehaviour {
     public float attackInterval = 1f; // Intervalo de tempo entre cada dano
     private float lastAttackTime;
 
+    private Animator animator;
+
+    public AudioClip damageSound;
+
     private void OnCollisionStay(Collision collision) {
         if (collision.collider.CompareTag("Player")) {
             if (Time.time - lastAttackTime >= attackInterval) {
-                //Devo chamar a animação aqui
-                collision.collider.GetComponent<PlayerLife>().TakeDamage(Damage);
+                animator.SetBool("Ataque", true);
+                // Som
+                
+                StartCoroutine(Atacando(collision.collider.GetComponent<PlayerLife>()));
                 lastAttackTime = Time.time;
             }
         }
+        
     }
 
+IEnumerator Atacando(PlayerLife playerLife){
+        yield return new WaitForSeconds(0.4f); // Espera a duração da animação de ataque
+        animator.SetBool("Ataque", false);
+        if(damageSound != null){
+                    AudioSource.PlayClipAtPoint(damageSound, transform.position);
+                }
+        playerLife.TakeDamage(Damage);    
+    }
     // Start is called before the first frame update
     void Start() {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
